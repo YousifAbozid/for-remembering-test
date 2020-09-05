0d2d0ba92685c95684269811b9dd8fb6c259cd81
Yousif Abozid <yousif.abozid@yahoo.com>
2020-09-05 16:23:11 +0200

    1.7 exercise

---
+++ 1.7/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleGood = () => setGood(good+1)
+  const handleNuetral = () => setNeutral(neutral+1)
+  const handleBad = () => setBad(bad+1)
+  const total = good + neutral + bad;
+  const average =
+    total === 0 ? 0 : (good * 1 + neutral * 0 - bad * 1) / total;
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNuetral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
