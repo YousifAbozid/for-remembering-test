2e46de6e7a905d16bf9ebfcc4a67a3a0dfbdb923
Yousif Abozid <yousif.abozid@yahoo.com>
2020-09-11 08:48:01 +0200

    2.7 exercise

---
+++ part 2/the-phonebook/2.7/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    const newObject = {
+      name: newName
+    }
+
+    if (Boolean(persons.find(person => person.name === newName))) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(newObject))
+      setNewName('')
+    } 
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const personsToShow = persons.map(person =>
+    person.name
+    )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName} >
+        <div>
+          name: <input value={newName}
+          onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsToShow.map(person =>
+        <Person key={person} person={person} />
+      )}
+    </div>
+  )
+}
+
+export default App
