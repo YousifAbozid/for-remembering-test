95f276a3d3273b4cf1b854727338e6e6c26c8864
Yousif Abozid <yousif.abozid@yahoo.com>
2020-09-17 15:31:33 +0200

    2.18 exercise

---
+++ part 2/the-phonebook/2.18/src/App.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+const Search = ({persons, searchName, handleSearch}) => {
+  const result = persons.filter(person => person.name.toUpperCase() === searchName.toUpperCase())
+  return (
+    <div>
+    filter shown with <input value={searchName}
+     onChange={handleSearch}/>
+    {result.map(person =>
+    <p key={person.name}><Person key={person.name} person={person} /></p>)}
+    </div>
+  )
+}
+
+const PersonForm = ({newName, newNumber, handleSubmit, handleNameChange, handleNewNumberChange}) => {
+  return (
+    <form onSubmit={handleSubmit} >
+    <div>
+      name: <input value={newName}
+      onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber}
+      onChange={handleNewNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+const Persons = ({persons, handleDelete}) => {
+  return (
+    <div>
+      {persons.map(person => {
+        return (
+          <div key={person.name}>
+            <p>
+              <Person person={person} />
+              <button onClick={() => handleDelete(person.id)}>delete</button>
+            </p>
+          </div>
+        )})}
+    </div>
+  )
+}
+
+const Person = ({ person }) => {
+  return <> {person.name} {person.number} </>
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchName, setSearchName ] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    
+    const newObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (Boolean(persons.find(person => person.name === newName))) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(person => person.name === newName)
+        //console.log("person is", person)
+        const changedNumber = {...person, number: newNumber}
+        //console.log(changedNumber)
+        personService
+          .update(person.id, changedNumber)
+          .then(returnedPerson => {
+            //console.log(returnedPerson)
+            //console.log(persons)
+            setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+      })
+    }
+    } else {
+    personService
+      .create(newObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }}
+
+  const handleDelete = (id) => {
+    const person = persons.find(person => person.id === id)
+    if (window.confirm(`delete ${person.name} ?`)) {
+      return (
+        personService
+          .deletion(id)
+          .then(data => {
+            setPersons(persons.filter(person => person.id !== id))
+          })
+      )
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Search persons={persons} searchName={searchName} handleSearch={handleSearch} />
+
+      <h3>add a new</h3>
+      <PersonForm newName={newName} newNumber={newNumber} handleSubmit={handleSubmit}
+      handleNameChange={handleNameChange} handleNewNumberChange={handleNewNumberChange} />
+
+      <h3>Numbers</h3>
+      <Persons persons={persons} handleDelete={handleDelete} />
+    </div>
+  )
+}
+
+export default App
