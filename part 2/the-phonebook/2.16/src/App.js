de515d0fdd608ad85bee14c70e520f7605db42d3
Yousif Abozid <yousif.abozid@yahoo.com>
2020-09-17 05:04:32 +0200

    2.16 exercise

---
+++ part 2/the-phonebook/2.16/src/App.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+const Search = ({persons, searchName, handleSearch}) => {
+  const result = persons.filter(person => person.name.toUpperCase() === searchName.toUpperCase())
+  return (
+    <div>
+    filter shown with <input value={searchName}
+     onChange={handleSearch}/>
+    {result.map(person =>
+    <Person key={person.name} person={person} />)}
+    </div>
+  )
+}
+
+const PersonForm = ({newName, newNumber, handleSubmit, handleNameChange, handleNewNumberChange}) => {
+  return (
+    <form onSubmit={handleSubmit} >
+    <div>
+      name: <input value={newName}
+      onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber}
+      onChange={handleNewNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+const Persons = ({persons}) => {
+  const personsToShow = persons.map(person => person)
+  return (
+    <div>
+      {personsToShow.map(person =>
+      <Person key={person.name} person={person} />)}
+    </div>
+  )
+}
+
+const Person = ({ person }) => <p>{person.name} {person.number}</p>
+
+const App = () => {
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchName, setSearchName ] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    
+    const newObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (Boolean(persons.find(person => person.name === newName))) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+        personService
+          .create(newObject)
+          .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setNewName('')
+            setNewNumber('')
+          })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Search persons={persons} searchName={searchName} handleSearch={handleSearch} />
+
+      <h3>add a new</h3>
+      <PersonForm newName={newName} newNumber={newNumber} handleSubmit={handleSubmit}
+      handleNameChange={handleNameChange} handleNewNumberChange={handleNewNumberChange} />
+
+      <h3>Numbers</h3>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+export default App
