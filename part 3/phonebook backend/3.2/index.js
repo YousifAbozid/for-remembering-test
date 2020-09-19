42dffbbca9b81939aaca91f81079b399e597bf05
Yousif Abozid <yousif.abozid@yahoo.com>
2020-09-19 23:02:37 +0200

    3.2 exercise

---
+++ part 3/phonebook backend/3.2/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    },
+    {
+        name: "yo",
+        number: "010",
+        id: 5
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+    console.log("hi")
+})
+
+app.get('/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people <br> <P>${new Date()}</P>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
