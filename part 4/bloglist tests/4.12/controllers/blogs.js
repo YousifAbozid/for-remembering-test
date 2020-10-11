b0850b4e39119c7d8864e3712b99a47bd0a538e8
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-11 10:11:18 +0200

    4.12 exercise

---
+++ part 4/bloglist tests/4.12/controllers/blogs.js
@@ -0,0 +1,18 @@
+const blogsRouter = require('express').Router()
+const Blog = require("../models/blog")
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+    const blog = new Blog(request.body)
+    if (!request.body.title || !request.body.url) {
+        return response.status(400).end()
+    }
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+})
+
+module.exports = blogsRouter
