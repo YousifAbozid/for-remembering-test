8fee07efca9123ab84ea25bc783d7f1ad0477fca
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-11 10:32:39 +0200

    4.13 exercise

---
+++ part 4/blog list expansions/4.13/utils/middleware.js
@@ -0,0 +1,29 @@
+const logger = require('./logger')
+
+/* you can comment this middleware "requestLogger" if you you don't wanna it logging to the console with the info,
+ also if you wanna logging to the console, you can use instead morgan library for logging to the console also,
+ you can find the link here and follow the instructions : https://github.com/expressjs/morgan */
+
+ const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+  
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
