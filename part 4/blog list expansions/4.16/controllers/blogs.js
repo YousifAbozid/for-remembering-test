be5a71e6411f795704eddb22f72faa9b93be93b5
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-15 00:54:24 +0200

    4.16 exercise

---
+++ part 4/blog list expansions/4.16/controllers/blogs.js
@@ -0,0 +1,33 @@
+const blogsRouter = require('express').Router()
+const Blog = require("../models/blog")
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const blog = new Blog(request.body)
+    if (!request.body.title || !request.body.url) {
+        return response.status(400).end()
+    }
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const blog = {
+        likes: body.likes
+    }
+  
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.status(200).json(updatedBlog.toJSON())
+})
+
+module.exports = blogsRouter
