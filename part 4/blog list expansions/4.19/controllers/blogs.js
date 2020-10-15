257c935467367a683180b44d63da83574982342f
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-15 04:50:27 +0200

    4.19 exercise

---
+++ part 4/blog list expansions/4.19/controllers/blogs.js
@@ -0,0 +1,59 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const Blog = require("../models/blog")
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const blog = {
+        likes: body.likes
+    }
+  
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.status(200).json(updatedBlog.toJSON())
+})
+
+module.exports = blogsRouter
