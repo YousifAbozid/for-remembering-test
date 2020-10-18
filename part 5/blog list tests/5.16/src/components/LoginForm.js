997e651719a1c533aeb22814b13a5f30986fab46
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-18 04:25:45 +0200

    5.16 exercise

---
+++ part 5/blog list tests/5.16/src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+const LoginForm = ({loginUser}) => {
+    const [username, setUsername] = useState('') 
+    const [password, setPassword] = useState('') 
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        const user = await loginUser({
+          username, password,
+        })
+        // checks if the user successfuly logged in, so it only sets the username and the password to '' after successful
+        // loging in, if not keep the data in the input field to not making the user typing everything again.
+        if (user) {
+            setUsername('')
+            setPassword('')
+        }
+      }
+
+    return (
+        <form onSubmit={handleLogin}>
+            <div>
+                username
+                <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password
+                <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>      
+    )
+}
+
+export default LoginForm
