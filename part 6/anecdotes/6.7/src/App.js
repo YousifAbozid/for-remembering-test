6a13529a1ef4776fec6a5bf9997128b0d0e0167d
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-23 10:19:38 +0200

    6.7 exercise

---
+++ part 6/anecdotes/6.7/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+import { vote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      )}
+      {AnecdoteForm()}
+    </div>
+  )
+}
+
+export default App
