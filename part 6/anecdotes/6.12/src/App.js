3c829e77d980168d5a649ce1c2a1d5583bc0c909
Yousif Abozid <yousif.abozid@yahoo.com>
2020-10-25 03:49:39 +0200

    6.12 exercise

---
+++ part 6/anecdotes/6.12/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Notification from './components/Notification'
+import Filter from './components/Filter'
+
+const App = () => {
+  const notification = useSelector(state => state.notification)
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {notification && <Notification />}
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
